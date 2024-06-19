235391bfa6edafaafef216cc7883ef9dd81ef522
Chandra Prakash <Chandra.Prakash@telusinternational.com>
2024-06-19 13:28:49 +0530

    added-code-for-increment-decrement-by-handle-using-state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const date = new Date();
-  return (
-    <div>
-      <h1>Name: Chandra Prakash</h1>
-      <h2>Today Date: {date.getDate()}</h2>
-      <h3>Institute: UTU</h3>
-      <h4>JS Operation: {2+3}</h4>
-    </div>
-  );
+
+  const [value, setValue] = useState(0);
+
+  const handleClick = (num , operation) => {
+    if(operation === "increment") {
+      setValue(value + num);
+    } else if(operation === "decrement") {
+       if(value>0){
+        setValue(value - num);
+       }
+    }
+  }
+
+  return <div>
+    <h1>Value Change here: {value}</h1>
+    <h2>Increment By: </h2>
+    <button onClick={() => handleClick(5 , "increment")}>Increment By 5</button>
+    <button onClick={() => handleClick(10, "increment")}>Increment By 10</button>
+    <button onClick={() => handleClick(15, "increment")}>Increment By 15</button>
+    <button onClick={() => handleClick(20, "increment")}>Increment By 20</button>
+    <hr />
+    <h2>Decrement By:</h2>
+    <button onClick={() => handleClick(5, "decrement")}>Decrement By 5</button>
+    <button onClick={() => handleClick(10, "decrement")}>Decrement By 10</button>
+    <button onClick={() => handleClick(15, "decrement")}>Decrement By 15</button>
+    <button onClick={() => handleClick(20, "decrement")}>Decrement By 20</button>
+  </div>
+ 
 }
 
 export default App;
