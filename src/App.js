a9fd880c3e70ff1601587c5f287ef53f260caad0
Chandra Prakash <Chandra.Prakash@telusinternational.com>
2024-06-20 17:27:29 +0530

    useRef-with-React-style

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useRef } from 'react';
 import './App.css';
+
+/**
+ * 
+ * Use of useRef hooks
+ * Use of Inline styling
+ * Use of CSS stylesheets
+ * Use of CSS Modules
+ * 
+ */
 
 function App() {
 
-  const [value, setValue] = useState(0);
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
 
-  const handleClick = (num , operation) => {
-    if(operation === "increment") {
-      setValue(value + num);
-    } else if(operation === "decrement") {
-       setValue(value - num);
-    }
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
 
-  return <div>
-    <h1>Value Change here: <span className="inc-dec-value">{value}</span></h1>
-    <h2>Increment By: </h2>
-    <button onClick={() => handleClick(5 , "increment")}>Increment By 5</button>
-    <button onClick={() => handleClick(10, "increment")}>Increment By 10</button>
-    <button onClick={() => handleClick(15, "increment")}>Increment By 15</button>
-    <button onClick={() => handleClick(20, "increment")}>Increment By 20</button>
-    <button onClick={() => handleClick(30, "increment")}>Increment By 30</button>
-    <hr />
-    <h2>Decrement By:</h2>
-    <button onClick={() => handleClick(5, "decrement")}>Decrement By 5</button>
-    <button onClick={() => handleClick(10, "decrement")}>Decrement By 10</button>
-    <button onClick={() => handleClick(15, "decrement")}>Decrement By 15</button>
-    <button onClick={() => handleClick(20, "decrement")}>Decrement By 20</button>
-    <button onClick={() => handleClick(30, "decrement")}>Decrement By 30</button>
-  </div>
- 
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" ref={usernameRef} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" ref={passwordRef} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
 }
 
 export default App;
